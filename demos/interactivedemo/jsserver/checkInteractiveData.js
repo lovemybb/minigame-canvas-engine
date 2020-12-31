e0227473f10a62761b49fcbbfe891d21672109fd
lianpeng <lianpeng>
2020-12-31 16:56:05 +0800

    代码格式

--- demos/interactivedemo/jsserver/checkInteractiveData.js
+++ demos/interactivedemo/jsserver/checkInteractiveData.js
@@ -1,103 +1,106 @@
-exports.main = function(arg) {
-    try {
-        arg = JSON.parse(arg);
-        const myOpenid         = wx.getOpenId();
-        const toOpenid         = arg.toUser;
-        const opNum            = arg.opNum;
-        const now              = new Date();
-        const inviteStorageKey = 'invite';
-        const friendsStorage   = wx.getFriendUserStorage([inviteStorageKey]);
+exports.main = function (arg) {
+  try {
+    arg = JSON.parse(arg);
+    const myOpenid = wx.getOpenId();
+    const toOpenid = arg.toUser;
+    const opNum = arg.opNum;
+    const now = new Date();
+    const inviteStorageKey = 'invite';
+    const friendsStorage = wx.getFriendUserStorage([inviteStorageKey]);
 
-        // 过滤和邀请不相关的用户
-        const userList         = friendsStorage.user_item.filter( item => item.kv_list && item.kv_list.length);
-        let ok                 = false;
+    // 过滤和邀请不相关的用户
+    const userList = friendsStorage.user_item.filter(item => item.kv_list && item.kv_list.length);
+    let ok = false;
 
-        // 用户每天只能给同一个好友赠送一次金币,每天最多送5次
-        const friendData = userList.find(userItem => userItem.openid === toOpenid) || { kv_list: []};
-        const myData     = userList.find(userItem => userItem.openid === myOpenid) || { kv_list: []};
+    // 用户每天只能给同一个好友赠送一次金币,每天最多送5次
+    const friendData = userList.find(userItem => userItem.openid === toOpenid) || {
+      kv_list: []
+    };
+    const myData = userList.find(userItem => userItem.openid === myOpenid) || {
+      kv_list: []
+    };
 
-        const friendKV = friendData.kv_list[friendData.kv_list.length - 1];
-        const selfKV   = myData.kv_list[myData.kv_list.length - 1];
+    const friendKV = friendData.kv_list[friendData.kv_list.length - 1];
+    const selfKV = myData.kv_list[myData.kv_list.length - 1];
 
-        let friendGift = friendKV && friendKV.value
-        let selfGift   = selfKV && selfKV.value;
+    let friendGift = friendKV && friendKV.value
+    let selfGift = selfKV && selfKV.value;
 
-        // 好友的记录
-        if (friendGift) {
-            friendGift = JSON.parse(friendGift)
-        } else {
-            friendGift = {
-                // 被邀请记录
-                beInvitedRecords: [],
-                // 邀请成功记录
-                inviteRecords   : [],
-            }
-        }
-
-        // 我的记录
-        if (selfGift) {
-            selfGift = JSON.parse(selfGift)
-        } else {
-            selfGift = {
-                // 被邀请记录
-                beInvitedRecords: [],
-                // 邀请成功记录
-                inviteRecords   : [],
-            }
-        }
-
-        // 判断是否已经邀请成功过
-        const hasInvite = friendGift.inviteRecords.find(item => item.openid === myOpenid );
-        if ( hasInvite ) {
-            ok = false;
-            // 验证通过
-            return JSON.stringify({
-                "ret": false
-            });
-        } else {
-            friendGift.inviteRecords.push({
-                openid: myOpenid,
-                time  : Date.now()
-            });
-
-            selfGift.beInvitedRecords.push({
-                openid: toOpenid,
-                time      : Date.now()
-            });
-
-            // 写对方的数据
-            let ret1 = wx.setFriendUserStorage(toOpenid, [{
-                key  : inviteStorageKey,
-                value: JSON.stringify(friendGift)
-            }]);
-
-            // 写自己的数据
-            let ret2 = wx.setFriendUserStorage(myOpenid, [{
-                key: inviteStorageKey,
-                value: JSON.stringify(selfGift)
-            }]);
-
-            if (ret1.errcode == 0 && ret2.errcode == 0) {
-                ok = true
-            } else {
-                console.error('fail');
-            }
-
-            if (ok) {
-                // 验证通过
-                return JSON.stringify({
-                    "ret": true
-                });
-            } else {
-                // 验证不通过
-                return JSON.stringify({
-                    "ret": false
-                });
-            }
-        }
-
-    } catch(err) {
-        console.error(err.message);
+    // 好友的记录
+    if (friendGift) {
+      friendGift = JSON.parse(friendGift)
+    } else {
+      friendGift = {
+        // 被邀请记录
+        beInvitedRecords: [],
+        // 邀请成功记录
+        inviteRecords: [],
+      }
     }
-}
 
+    // 我的记录
+    if (selfGift) {
+      selfGift = JSON.parse(selfGift)
+    } else {
+      selfGift = {
+        // 被邀请记录
+        beInvitedRecords: [],
+        // 邀请成功记录
+        inviteRecords: [],
+      }
+    }
+
+    // 判断是否已经邀请成功过
+    const hasInvite = friendGift.inviteRecords.find(item => item.openid === myOpenid);
+    if (hasInvite) {
+      ok = false;
+      // 验证通过
+      return JSON.stringify({
+        "ret": false
+      });
+    } else {
+      friendGift.inviteRecords.push({
+        openid: myOpenid,
+        time: Date.now()
+      });
+
+      selfGift.beInvitedRecords.push({
+        openid: toOpenid,
+        time: Date.now()
+      });
+
+      // 写对方的数据
+      let ret1 = wx.setFriendUserStorage(toOpenid, [{
+        key: inviteStorageKey,
+        value: JSON.stringify(friendGift)
+      }]);
+
+      // 写自己的数据
+      let ret2 = wx.setFriendUserStorage(myOpenid, [{
+        key: inviteStorageKey,
+        value: JSON.stringify(selfGift)
+      }]);
+
+      if (ret1.errcode == 0 && ret2.errcode == 0) {
+        ok = true
+      } else {
+        console.error('fail');
+      }
+
+      if (ok) {
+        // 验证通过
+        return JSON.stringify({
+          "ret": true
+        });
+      } else {
+        // 验证不通过
+        return JSON.stringify({
+          "ret": false
+        });
+      }
+    }
+
+  } catch (err) {
+    console.error(err.message);
+  }
+}
--- demos/interactivedemo/sub/index.js
+++ demos/interactivedemo/sub/index.js
@@ -1,23 +1,23 @@
-import style    from 'render/style.js';
-import tplFn    from 'render/tplfn.js';
-import Layout   from './engine.js'
+import style from 'render/style.js';
+import tplFn from 'render/tplfn.js';
+import Layout from './engine.js'
 
-let selfOpenId    = '';
-let sharedCanvas  = wx.getSharedCanvas();
+let selfOpenId = '';
+let sharedCanvas = wx.getSharedCanvas();
 let sharedContext = sharedCanvas.getContext('2d');
 
 function draw(data = [], invites) {
-    Layout.clearAll();
-    let template = tplFn({
-        title: '可能感兴趣好友',
-        data,
-        invites,
-    });
+  Layout.clearAll();
+  let template = tplFn({
+    title: '可能感兴趣好友',
+    data,
+    invites,
+  });
 
-    Layout.init(template, style);
-    Layout.layout(sharedContext);
+  Layout.init(template, style);
+  Layout.layout(sharedContext);
 
-    console.log(Layout);
+  console.log(Layout);
 }
 
 /**
@@ -25,96 +25,97 @@ function draw(data = [], invites) {
  * 这里把邀请记录拉下来筛选出我的邀请成功记录
  */
 function getInviteData(selfOpenId) {
-    return new Promise((resolve, reject) => {
-        wx.getFriendCloudStorage({
-            keyList: ['invite'],
-            success: ({data}) => {
-                console.log('getFriendCloudStorage', data);
-                data = data.filter( item => item.KVDataList && item.KVDataList.length);
-                let result = [];
+  return new Promise((resolve, reject) => {
+    wx.getFriendCloudStorage({
+      keyList: ['invite'],
+      success: ({
+        data
+      }) => {
+        console.log('getFriendCloudStorage', data);
+        data = data.filter(item => item.KVDataList && item.KVDataList.length);
+        let result = [];
 
-                let self = data.find(item => item.openid=== selfOpenId);
-                let selfData;
+        let self = data.find(item => item.openid === selfOpenId);
+        let selfData;
 
-                try {
-                    selfData = self && self.KVDataList && self.KVDataList[0] && self.KVDataList[0].value && JSON.parse(self.KVDataList[0].value);
-                } catch(e) {
-                    console.error(e);
-                    selfData = null;
-                }
+        try {
+          selfData = self && self.KVDataList && self.KVDataList[0] && self.KVDataList[0].value && JSON.parse(self.KVDataList[0].value);
+        } catch (e) {
+          console.error(e);
+          selfData = null;
+        }
 
-                if ( selfData ) {
-                    result = data.filter( item => selfData.inviteRecords.find(r => r.openid === item.openid ))
-                }
+        if (selfData) {
+          result = data.filter(item => selfData.inviteRecords.find(r => r.openid === item.openid))
+        }
 
-                resolve(result);
-            }
-        });
+        resolve(result);
+      }
     });
+  });
 }
 
 
 function showPotentialFriend(invites) {
-    wx.getPotentialFriendList({
-        success: (data) => {
-            let list = data.list || [];
+  wx.getPotentialFriendList({
+    success: (data) => {
+      let list = data.list || [];
 
-            console.log('getPotentialFriendList', data);
+      console.log('getPotentialFriendList', data);
 
-            let friends = list.map( (item, i) => {
-                return {
-                    rank      : i,
-                    rankScore : 0,
-                    nickname  : item.nickname,
-                    avatarUrl : item.avatarUrl,
-                    openid    : item.openid,
-                }
-            });
-
-            draw(friends, invites);
-
-            Layout.getElementsByClassName('listButton').forEach( (item, index) => {
-                item.on('click', (e) => {
-                    wx.shareMessageToFriend({
-                        openId  : friends[index].openid,
-                        title   : '快来探索浩瀚星空',
-                        imageUrl: 'https://mmocgame.qpic.cn/wechatgame/TKicR7oWel4znvwMdwOpuwoy1ntVB44kT9ZSse2u0xNcO5SaxIS2UwU0GdUfA2Al0/0'
-                    })
-                });
-            });
+      let friends = list.map((item, i) => {
+        return {
+          rank: i,
+          rankScore: 0,
+          nickname: item.nickname,
+          avatarUrl: item.avatarUrl,
+          openid: item.openid,
         }
-    });
+      });
+
+      draw(friends, invites);
+
+      Layout.getElementsByClassName('listButton').forEach((item, index) => {
+        item.on('click', (e) => {
+          wx.shareMessageToFriend({
+            openId: friends[index].openid,
+            title: '快来探索浩瀚星空',
+            imageUrl: 'https://mmocgame.qpic.cn/wechatgame/TKicR7oWel4znvwMdwOpuwoy1ntVB44kT9ZSse2u0xNcO5SaxIS2UwU0GdUfA2Al0/0'
+          })
+        });
+      });
+    }
+  });
 }
 
 
 wx.onMessage(data => {
-    console.log(data);
+  console.log(data,'wx.onMessage');
 
-    if ( data.event === 'getOpenId' ) {
-        selfOpenId = data.value;
+  if (data.event === 'getOpenId') {
+    selfOpenId = data.value;
 
-        getInviteData(selfOpenId).then( invites => {
-            showPotentialFriend(invites);
-        });
-    } else if ( data.event === 'refreshData' ) {
-        getInviteData(selfOpenId).then( invites => {
-            showPotentialFriend(invites);
-        });
-    } else if ( data.event === 'updateViewPort' ) {
-        Layout.updateViewPort(data.box);
-    } else if ( data.event === 'close' ) {
-        Layout.clearAll();
-    } else if ( data.event === 'reciveInvite' ) {
-        wx.modifyFriendInteractiveStorage({
-            key      :'2',
-            opNum    : 1,
-            operation: 'add',
-            // 静默修改需要用户通过快捷分享消息卡片进入才有效，代表分享反馈操作，无需填写 toUser，直接修改分享者与被分享者交互数据
-            quiet    : true,
-            complete:(res) => {
-                console.log('modifyFriendInteractiveStorage', res);
-            }
-        })
-    }
+    getInviteData(selfOpenId).then(invites => {
+      showPotentialFriend(invites);
+    });
+  } else if (data.event === 'refreshData') {
+    getInviteData(selfOpenId).then(invites => {
+      showPotentialFriend(invites);
+    });
+  } else if (data.event === 'updateViewPort') {
+    Layout.updateViewPort(data.box);
+  } else if (data.event === 'close') {
+    Layout.clearAll();
+  } else if (data.event === 'reciveInvite') {
+    wx.modifyFriendInteractiveStorage({
+      key: '2',
+      opNum: 1,
+      operation: 'add',
+      // 静默修改需要用户通过快捷分享消息卡片进入才有效，代表分享反馈操作，无需填写 toUser，直接修改分享者与被分享者交互数据
+      quiet: true,
+      complete: (res) => {
+        console.log('modifyFriendInteractiveStorage', res);
+      }
+    })
+  }
 });
-
